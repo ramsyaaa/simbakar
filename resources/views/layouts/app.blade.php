f3a1b1f3969e782772951715043fa32070da7a2f
rizky14045 <mrizkysaputra17@gmail.com>
2024-10-05 05:44:22 +0700

    add excel fitur

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -280,6 +280,17 @@
             return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
         }
     </script>
+   <script type="text/javascript" src="{{asset('js/xlsx.full.min.js')}}"></script>
+   <script>
+       function ExportToExcel(type, fn, dl) {
+          var elt = document.getElementById('table');
+          var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+          return dl ?
+            XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
+            XLSX.writeFile(wb, fn || ('Document.' + (type || 'xlsx')));
+       }
+   
+   </script>
 </body>
 
 </html>
